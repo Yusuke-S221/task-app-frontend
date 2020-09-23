794f5bd40dcdac8b01a97ef90343aa6a34e2c052
YusukeSato <yskprog022151@gmail.com>
2020-09-23 16:00:16 +0900

    ヘッダー作成

---
+++ components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <nuxt-link to="/">プロジェクト一覧</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+  padding: 20px;
+  background-color: #527b8e;
+}
+
+ul {
+  display: flex;
+}
+
+li {
+  list-style: none;
+}
+
+a {
+  color: aliceblue;
+}
+</style>
