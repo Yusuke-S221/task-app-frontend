3114f5ee1095f3bb21a5638e5bdbf39e99d9b2c1
YusukeSato <yskprog022151@gmail.com>
2020-10-07 11:56:06 +0900

    css調整

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,37 @@
 <template>
   <div>
     <Header />
-    プロジェクト一覧
-    <div v-for="project in projects" :key="project.id" :project="project">
-      <Card>
-        <ProjectCard>{{ project.title }}</ProjectCard>
-      </Card>
+    <Title>プロジェクト一覧</Title>
+    <el-button type="primary" class="create-button" @click="createProject"
+      >新規作成</el-button
+    >
+    <div class="contents">
+      <div
+        v-for="project in projects"
+        :key="project.id"
+        :project="project"
+        class="project-cards"
+      >
+        <Card>
+          <ProjectCard>
+            <div class="project-content">
+              <div class="project-title">
+                <h3>{{ project.title }}</h3>
+              </div>
+              <div class="project-description">
+                {{ project.description }}
+              </div>
+            </div>
+          </ProjectCard>
+        </Card>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Header from "~/components/Header.vue";
+import Title from "~/components/Title.vue";
 import Card from "~/components/Card.vue";
 import ProjectCard from "~/components/ProjectCard.vue";
 
@@ -32,4 +52,22 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.create-button {
+  margin-left: 60px;
+  font-size: 16px;
+  font-weight: bold;
+}
+
+.contents {
+  margin: 30px;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.project-cards {
+  width: 48%;
+  margin: 10px;
+  box-sizing: border-box;
+}
+</style>
