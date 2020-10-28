31ac770da816542049e7eb74c788ac4050737c58
YusukeSato <yskprog022151@gmail.com>
2020-10-28 11:45:04 +0900

    issue一覧ページ作成

---
+++ pages/issues/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-loading.fullscreen.lock="fullscreenLoading"> 
+    <Header />
+    <div class="page-title">
+      <Title>Issue一覧</Title>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "~/components/Header.vue";
+import Title from "~/components/Title.vue";
+
+export default {
+  components: {
+    Header,
+    Title
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ test/pages/issues/index.spec.js
@@ -0,0 +1,51 @@
+import { shallowMount, RouterLinkStub } from "@vue/test-utils";
+import Issues from "~/pages/issues/index.vue";
+import flushPromises from "flush-promises";
+
+const issues = [
+  {
+    id: 1,
+    ordering: 1,
+    title: 'テスト1',
+    description: '内容1',
+    state: 'wip',
+    project_id: 1,
+    created_at: '2020-01-01 01:01:01',
+    updated_at: '2020-01-01 01:01:01'
+  },
+  {
+    id: 2,
+    ordering: 2,
+    title: 'テスト2',
+    description: '内容2',
+    state: 'wip',
+    project_id: 2,
+    created_at: '2020-01-01 01:01:01',
+    updated_at: '2020-01-01 01:01:01'
+  },
+  {
+    id: 3,
+    ordering: 3,
+    title: 'テスト3',
+    description: '内容3',
+    state: 'wip',
+    project_id: 3,
+    created_at: '2020-01-01 01:01:01',
+    updated_at: '2020-01-01 01:01:01'
+  }
+]
+
+describe('Issues', () => {
+  let wrapper 
+  beforeEach(() => {
+    wrapper = shallowMount(Issues, {
+      stubs: {
+        NuxtLink: RouterLinkStub
+      }
+    })
+  })
+
+  it("is a vue instance", () => {
+    expect(wrapper.vm).toBeTruthy();
+  });
+})
