7a076fa7e6fc8c0d6c23354dd542ca21b3197a4b
YusukeSato <yskprog022151@gmail.com>
2020-10-28 17:56:10 +0900

    createIssueメソッドテスト作成

--- test/pages/issues/index.spec.js
+++ test/pages/issues/index.spec.js
@@ -110,4 +110,119 @@ describe('Issues', () => {
       )
     })
   })
+
+  describe("method createIssue", () => {
+    describe("success response", () => {
+      it("calls method startLoading", async () => {
+        wrapper.vm.startLoading = jest.fn()
+        wrapper.vm.createIssue();
+        await flushPromises()
+        expect(wrapper.vm.startLoading).toHaveBeenCalled()
+      });
+
+      it("calls POST http://localhost:80/api/issues", async () => {
+        wrapper.vm.$data.form = {
+          title: 'テスト',
+          description: '内容'
+        }
+        wrapper.vm.createIssue();
+        await flushPromises()
+        expect(mockAxios.$post).toHaveBeenCalledWith(
+          "http://localhost:80/api/issues", 
+          {
+            title: 'テスト',
+            description: '内容'
+          }
+        );
+      });
+
+      it("calls method fetchIssues", async () => {
+        wrapper.vm.fetchIssues = jest.fn()
+        wrapper.vm.createIssue();
+        await flushPromises()
+        expect(wrapper.vm.fetchIssues).toHaveBeenCalled()
+      });
+
+      it("calls message success", async () => {
+        wrapper.vm.createIssue();
+        await flushPromises()
+        expect(message).toHaveBeenCalled();
+      })
+
+      it("calls method closeCreateDialog", async () => {
+        wrapper.vm.closeCreateDialog = jest.fn()
+        wrapper.vm.createIssue();
+        await flushPromises()
+        expect(wrapper.vm.closeCreateDialog).toHaveBeenCalled()
+      });
+  
+      it("calls method endLoading", async () => {
+        wrapper.vm.endLoading = jest.fn()
+        wrapper.vm.createIssue();
+        await flushPromises()
+        expect(wrapper.vm.endLoading).toHaveBeenCalled()
+      });
+    })
+
+    describe("error response", () => {
+      let errorWrapper
+      beforeEach(() => {
+        errorWrapper = shallowMount(Issues, {
+          data() {
+            return data
+          },
+          mocks: {
+            $axios: errorMockAxios,
+            $message: errorMessage
+          },
+          stubs: {
+            NuxtLink: RouterLinkStub
+          }
+        });
+      })
+
+      it("calls method startLoading", async () => {
+        errorWrapper.vm.startLoading = jest.fn()
+        errorWrapper.vm.createIssue();
+        await flushPromises()
+        expect(errorWrapper.vm.startLoading).toHaveBeenCalled()
+      });
+
+      it("calls POST http://localhost:80/api/issues", async () => {
+        errorWrapper.vm.$data.form = {
+          title: 'テスト',
+          description: '内容'
+        }
+        errorWrapper.vm.createIssue();
+        await flushPromises()
+        expect(errorMockAxios.$post).toHaveBeenCalledWith(
+          "http://localhost:80/api/issues", 
+          {
+            title: 'テスト',
+            description: '内容'
+          }
+        );
+      });
+
+      it("calls message error", async () => {
+        errorWrapper.vm.createIssue();
+        await flushPromises()
+        expect(errorMessage.error).toHaveBeenCalled();
+      })
+
+      it("calls method closeCreateDialog", async () => {
+        errorWrapper.vm.closeCreateDialog = jest.fn()
+        errorWrapper.vm.createIssue();
+        await flushPromises()
+        expect(errorWrapper.vm.closeCreateDialog).toHaveBeenCalled()
+      });
+  
+      it("calls method endLoading", async () => {
+        errorWrapper.vm.endLoading = jest.fn()
+        errorWrapper.vm.createIssue();
+        await flushPromises()
+        expect(errorWrapper.vm.endLoading).toHaveBeenCalled()
+      });
+    });
+  });
 })
